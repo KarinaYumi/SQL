f5e6306ddac313b7c8747a37440f68c757971027
Karina Yumi Ferreira <92693545+KarinaYumi@users.noreply.github.com>
2022-04-01 20:43:41 -0300

    Create bdEscola.sql

---
+++ Escola/bdEscola.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE bdEscola
+GO
+USE bdEscola
+
+CREATE TABLE tbAluno(
+	codAluno INT PRIMARY KEY IDENTITY(1,1)
+	,nomeAluno VARCHAR (90) 
+	,dataNascAluno SMALLDATETIME 
+	,rgAluno VARCHAR (15) 
+	,natuAluno VARCHAR (2) 
+);
+
+CREATE TABLE tbCurso(
+	codCurso INT PRIMARY KEY IDENTITY(1,1)
+	,nomeCurso VARCHAR (90) 
+	,cargaHorariaCurso INT
+	,valorCurso MONEY
+);
+
+CREATE TABLE tbTurma (
+	codTurma INT PRIMARY KEY IDENTITY (1,1)
+	,nomeTurma VARCHAR (90)
+	,codCurso INT FOREIGN KEY REFERENCES tbCurso(codCurso)
+	,horarioTurma SMALLDATETIME
+);
+
+CREATE TABLE tbMatricula(
+	codMatricula INT PRIMARY KEY IDENTITY(1,1)
+	,dataMatricula SMALLDATETIME
+	,codAluno INT FOREIGN KEY REFERENCES tbAluno(codAluno)
+	,codTurma INT FOREIGN KEY REFERENCES tbTurma(codTurma)
+);
+
