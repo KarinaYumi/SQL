c8811acc94821be896dc91b4a8fcbbb151e6e896
Karina Yumi Ferreira <92693545+KarinaYumi@users.noreply.github.com>
2022-09-27 19:44:55 -0300

    Update trigger_bdTransito.sql

--- Transito/trigger_bdTransito.sql
+++ Transito/trigger_bdTransito.sql
@@ -1,5 +1,5 @@
--- Criar uma trigger que ao veÌculo tomar uma multa os pontos da multa sejam  atualizados na tabela do motorista no campo pontuacaoAcumulada. 
--- Caso o motorista alcance 20 pontos informar via mensagem que o mesmo poder· ter sua habilitaÁ„o suspensa.
+-- Criar uma trigger que ao ve√≠culo tomar uma multa os pontos da multa sejam  atualizados na tabela do motorista no campo pontuacaoAcumulada. 
+-- Caso o motorista alcance 20 pontos informar via mensagem que o mesmo poder√° ter sua habilita√ß√£o suspensa.
 
 CREATE TRIGGER tgMulta
 ON tbMultas AFTER INSERT
@@ -12,19 +12,19 @@ AS
 	SET @idMulta = (SELECT idMulta FROM INSERTED)
 	SET @pontos = (SELECT pontosMulta FROM INSERTED)
 	SET @idMotorista = (SELECT tbMotorista.idMotorista FROM tbMotorista 
-						INNER JOIN tbVeiculo ON tbMotorista.idMotorista = tbVeiculo.idMotorista 
-						INNER JOIN tbMultas ON tbVeiculo.idVeiculo = tbMultas.idVeiculo WHERE idMulta = @idMulta)
+		INNER JOIN tbVeiculo ON tbMotorista.idMotorista = tbVeiculo.idMotorista 
+		INNER JOIN tbMultas ON tbVeiculo.idVeiculo = tbMultas.idVeiculo WHERE idMulta = @idMulta)
 	UPDATE tbMotorista 
 	SET pontuacaoAcumulada = pontuacaoAcumulada + @pontos
 	WHERE idMotorista = @idMotorista
 
 	IF @pontuacaoAcumulada >= 20 
 		BEGIN
-			PRINT ('VOC  ATINGIU +20 PONTOS NA CARTEIRA, SUA CARTA PODER¡ SER SUSPENSA!!!')
+			PRINT ('VOC√ä ATINGIU +20 PONTOS NA CARTEIRA, SUA CARTA PODER√Å SER SUSPENSA!!!')
 		END
 	ELSE
 		BEGIN
-			PRINT('VOC  TEM '+@pontuacaoAcumulada+'PONTOS NA CARTEIRA!')
+			PRINT('VOC√ä TEM '+@pontuacaoAcumulada+'PONTOS NA CARTEIRA!')
 		END
 GO
 
